2e6d448522706ba319761683eb645f1c14fe2699
Tianhao Zhuang <zhuangdeyouxiang@gmail.com>
2015-06-22 16:13:04 +1000

    unsubscribe

--- Intro2Rx/Program.cs
+++ Intro2Rx/Program.cs
@@ -10,30 +10,24 @@ namespace Intro2Rx
     {
         static void Main(string[] args)
         {
-            /*
-             * 
-            var values = new Subject<int>();
-            try
-            {
-                values.Subscribe(value => Console.WriteLine("1st subscription received {0}", value));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Won't catch anything here!");
-            }
-            values.OnNext(0);
-            //Exception will be thrown here causing the app to fail.
-            values.OnError(new Exception("Dummy exception"));
-             */
-            
             var values = new Subject<int>();
             
-            values.Subscribe(
-                value => Console.WriteLine("1st subscription received {0}", value),
-                ex => Console.WriteLine("Caught an exception : {0}", ex));
+            var firstSubscription = values.Subscribe(value =>
+            Console.WriteLine("1st subscription received {0}", value));
+
+            var secondSubscription = values.Subscribe(value =>
+            Console.WriteLine("2nd subscription received {0}", value));
             
             values.OnNext(0);
-            values.OnError(new Exception("Dummy exception"));
+            values.OnNext(1);
+            values.OnNext(2);
+            values.OnNext(3);
+
+            firstSubscription.Dispose();
+            Console.WriteLine("Disposed of 1st subscription");
+            
+            values.OnNext(4);
+            values.OnNext(5);
         }
 
         //Takes an IObservable<string> as its parameter. 
