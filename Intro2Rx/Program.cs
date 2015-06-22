74f2ea8ad9f2cc16a4e39a2296086364330cdcef
Tianhao Zhuang <zhuangdeyouxiang@gmail.com>
2015-06-22 17:24:57 +1000

    Observable.Return

--- Intro2Rx/Program.cs
+++ Intro2Rx/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Threading;
 
@@ -10,25 +11,17 @@ namespace Intro2Rx
     {
         static void Main(string[] args)
         {
-//            var subject = new Subject<int>();
-//         
-//            subject.Subscribe(
-//                Console.WriteLine,
-//                () => Console.WriteLine("Completed"));
-//
-//            subject.OnCompleted();
-//            subject.OnNext(2);
+//            var singleValue = Observable.Return<string>("Value");
+            
+            //Can be reduced to the following
+            var singleValue = Observable.Return("Value");
 
-            Console.WriteLine("Normal color");
-            using (new ConsoleColor(System.ConsoleColor.Red))
-            {
-                Console.WriteLine("Now I am Red");
-                using (new ConsoleColor(System.ConsoleColor.Green))
-                {
-                    Console.WriteLine("Now I am Green");
-                }
-                Console.WriteLine("and back to Red");
-            }
+            //Equivalent to, below:
+
+            //which could have also been simulated with a replay subject
+            var subject = new ReplaySubject<string>();
+            subject.OnNext("Value");
+            subject.OnCompleted();
         }
 
         // Takes an IObservable<string> as its parameter. 
