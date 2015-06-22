095116a1f828ec4087f117f63a18e0e761181278
Tianhao Zhuang <zhuangdeyouxiang@gmail.com>
2015-06-22 17:51:49 +1000

    BUGFIX: dispose inner timer too, when subscription is disposed.

--- Intro2Rx/Program.cs
+++ Intro2Rx/Program.cs
@@ -27,8 +27,27 @@ namespace Intro2Rx
                 timer.Elapsed += (s, e) => observer.OnNext("tick"); 
                 timer.Elapsed += OnTimerElapsed;
                 timer.Start();
-        
-                return Disposable.Empty;
+                
+                //return timer as IDisposable token ??     
+                //Now when a consumer disposes of their subscription, the underlying Timer will be disposed of too.
+
+                /*
+                 When we dispose of our subscription, we will stop seeing "tick" being written to the screen;
+                 
+                 * So output can be:
+                 *  tick
+                    01/01/2012 12:00:00
+                    tick
+                    01/01/2012 12:00:01
+                    tick
+                    01/01/2012 12:00:02
+                    01/01/2012 12:00:03
+                    01/01/2012 12:00:04
+                    01/01/2012 12:00:05
+                 */
+                return timer;
+
+//                return Disposable.Empty;
             });
 
             //simple subscription.
